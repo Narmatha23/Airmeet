f716bd298d6bbcd179ee11b82974eed645658a89
Narmatha23 <rr.narmathadevi@gmail.com>
2021-03-19 21:05:49 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -81,25 +81,5 @@ $('div').on('click',function(e) {
 });
 
 
-<script>
-        var swiper = new Swiper('.swiper-container', {
-          slidesPerView: 3,
-          direction: getDirection(),
-          navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          },
-          on: {
-            resize: function () {
-              swiper.changeDirection(getDirection());
-            }
-          }
-        });
-    
-        function getDirection() {
-          var windowWidth = window.innerWidth;
-          var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
-    
-          return direction;
-        }
-      </script>
+
+       
